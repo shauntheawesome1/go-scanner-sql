b15652c0494904e1e154bcb80a2c0cde4f503108
shauntheawesome1 <shaun.munshi@gmail.com>
2024-02-07 16:46:33 -0800

    Deleting example.go, adding main.go and scanner.go

---
+++ main.go
@@ -0,0 +1,10 @@
+// Filename: Main.go
+// Code for a scanner for MySQL. Code will be triggered from Main.go, and used to perform the scanning action.
+// Language: Go
+// To-do: MYSQL Handshake
+
+package main
+
+func main() {
+	println("Main Code Testing")
+}
---
+++ scanner.go
@@ -0,0 +1,15 @@
+// Filename: Scanner.go
+// Code for a scanner for MySQL. Code will be triggered from Main.go, and used to perform the scanning action.
+// Language: Go
+// To-do: MYSQL Handshake
+
+package main
+
+type arguments struct {
+	Host string
+	Port string
+}
+
+func init() {
+	println("Testing")
+}
