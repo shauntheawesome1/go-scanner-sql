cf6f5237ddef0f952c17286f750f3c400850320b
shauntheawesome1 <shaun.munshi@gmail.com>
2024-02-07 21:57:17 -0800

    Minor changes to Main's UI

--- main.go
+++ main.go
@@ -18,10 +18,18 @@ func main() {
 	fmt.Println("Enter Your IP Address: ")
 
 	fmt.Scanln(&ipaddr)
+	if ipaddr == "" || ipaddr == "localhost" || ipaddr == "default" {
+		ipaddr = "127.0.0.1"
+	}
 
 	fmt.Println("Enter Port # to Scan: ")
 
 	fmt.Scanln(&port_num)
+
+	if port_num < 1 || port_num > 65535 {
+		port_num = 3306
+	}
+	fmt.Println(ipaddr)
 	testPrint()
 
 }
