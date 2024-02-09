b2d7b4ec89453749de46efddf5696033a3101e0f
shauntheawesome1 <shaun.munshi@gmail.com>
2024-02-09 00:29:14 -0800

    Last commit from old Computer

--- scanner.go
+++ scanner.go
@@ -6,16 +6,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"strconv"
 	"time"
 )
 
 type ScanOutput struct {
-	Host  string
-	Port  int    // if no port specified, using port
-	State string // state of specified port
+	Host    string
+	Port    int    // if no port specified, using port
+	State   string // state of specified port
+	Message int    // connection message
 }
 
 type sql_evidence struct {
@@ -57,9 +61,67 @@ func scanPort(protocol, hostname string, port int) ScanOutput {
 		return output
 	}
 
-	defer conn.Close()
-	fmt.Println("Connection Closed")
-	output.State = "Open"
+	//defer conn.Close()
+	/*fmt.Println("Connection Closed")
+	buf := make([]byte, 4)
+	fmt.Println("Created Buffer")
+	output.Message, err = conn.Read(buf)
+	fmt.Println("Done Reading Connection")
+	output.State = "Open"*/
+
+	/*lenBuf := make([]byte, 4)
+	fmt.Println("Created Buffer")
+	output.Message, err = conn.Read(lenBuf)
+	fmt.Println("Done Reading Connection")
+	if err != nil {
+		output.State = "Closed"
+		return output
+	}*/
+
+	lenBuf := make([]byte, 4)
+	fmt.Println("Created Buffer1")
+	_, err = conn.Read(lenBuf)
+	fmt.Println("Connection Passed1")
+	if err != nil {
+		output.State = "Closed"
+		return output
+	}
+
+	lenData, err := msgLength(lenBuf)
+	if err != nil {
+		output.State = "Closed"
+		return output
+	}
+
+	/*buf := make([]byte, lenData)
+	output.Message, err = conn.Read(buf)
+	if err == io.EOF {
+		output.State = "Closed"
+		return output
+	}
+	if err != nil {
+		output.State = "Closed"
+		return output
+	}*/
+
+	buf := make([]byte, lenData)
+	fmt.Println("Created Buffer2")
+	_, err = conn.Read(buf)
+	fmt.Println("Connection Passed2")
+	if err == io.EOF {
+		output.State = "Closed"
+		return output
+	}
+	if err != nil {
+		output.State = "Closed"
+		return output
+	}
+
+	if err := conn.Close(); err != nil {
+		output.State = "Closed"
+		return output
+	}
+
 	return output
 }
 
@@ -73,4 +135,12 @@ func InitialScan(hostname string, port_num int) []ScanOutput {
 	fmt.Printf("Port Open: %d\n", port_num)
 
 	return results
+}
+
+func msgLength(b []byte) (int32, error) {
+	buf := bytes.NewReader(b)
+	var result int32
+	err := binary.Read(buf, binary.LittleEndian, &result)
+
+	return result, err
 }
