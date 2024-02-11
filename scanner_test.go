0e31f1713fcfb1667ec585f427dbd07dcfccc6ff
shauntheawesome1 <shaun.munshi@gmail.com>
2024-02-10 22:26:49 -0800

    Added Scanner_Test

---
+++ scanner_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/orlangure/gnomock"
+	"github.com/orlangure/gnomock/preset/mysql"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestScan(t *testing.T) {
+
+}
